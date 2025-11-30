5f3855932b7c40521ed507a9321f287964ba935a
kipling <kiplingmarich@gmail.com>
2025-11-30 16:00:35 +0300

    Add env copier to setup script

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,12 +4,12 @@ if [ ! -f "vendor/autoload.php" ]; then
     composer install --no-progress --no-interaction
 fi
 
-# if [ ! -f ".env" ]; then
-#     echo "Creating .env file for env $APP_ENV"
-#     cp .env.example .env
-# else
-#     echo ".env file exists"
-# fi
+if [ ! -f ".env" ]; then
+    echo "Creating .env file for env $APP_ENV"
+    cp .env.example .env
+else
+    echo ".env file exists"
+fi
 
 php artisan migrate
 php artisan key:generate
